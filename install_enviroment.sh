b31938a4a7d231e3193bfb3a19121e387f768ed3
Anibal <anibal.siguenza1@gmail.com>
2024-12-02 14:22:26 +0100

    add tmux installation

--- install_enviroment.sh
+++ install_enviroment.sh
@@ -9,6 +9,7 @@ install_vim () {
 }
 
 install_oh_my_shell() {
+    echo "Installing OH-MY-SHELL"
     sudo apt-get install zsh
     wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
     git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins
@@ -20,6 +21,12 @@ install_fzf () {
     ${SCRIPT_DIR}/fzf/install
 }
 
+install_tmux () {
+    echo "Installing TMUX"
+    sudo apt-get install tmux
+}
+
 install_vim
 install_oh_my_shell
 install_fzf 
+install_tmux 
